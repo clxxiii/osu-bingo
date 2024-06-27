0bd542990ad40d1b7493344e49ff1ac42eb6e2b0
clxxiii <clxxiii@users.noreply.github.com>
2024-06-27 00:43:32 -0400

    add very basic game card

---
+++ src/lib/components/GameListItem.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let game: Bingo.BingoGame;
+
+	const text = ['Starting soon', 'In Progress', 'Completed'];
+</script>
+
+<a
+	class=" bg-zinc-800 relative hover:shadow-lg transition p-2 h-24 rounded-xl flex"
+	href="/{game.link_id}"
+>
+	<div
+		data-state={`${game.state}`}
+		class="h-full w-1 mr-2 rounded data-[state='0']:bg-green-400 data-[state='1']:bg-blue-600 data-[state='2']:bg-zinc-600"
+	></div>
+	<div>
+		<h2 class="text-lg">
+			Game {game.link_id}
+		</h2>
+		<span class="absolute bottom-2 uppercase right-2 text-zinc-600">{text[game.state]}</span>
+	</div>
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
+	import GameListItem from '$lib/components/GameListItem.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<h1>Welcome to osu! Bingo!</h1>
-{#each data.games as game}
-	<div>
-		{JSON.stringify(game)}
+<svelte:head>
+	<title>osu!Bingo</title>
+</svelte:head>
+
+<div class="pattern h-[300px]"></div>
+<div class="m-auto mt-10 w-full max-w-[600px]">
+	<h1 class="text-3xl">Welcome to osu! Bingo!</h1>
+	Join a public game, or create your own!
+	<hr class="border-zinc-700 my-2" />
+	<div class="w-full">
+		{#each data.games as game}
+			<GameListItem {game} />
+		{/each}
 	</div>
-{/each}
+</div>
+
+<style>
+	.pattern {
+		background-color: #18181b;
+		background-image: url("data:image/svg+xml,%3Csvg width='80' height='80' viewBox='0 0 80 80' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%233f3f46' fill-opacity='0.4'%3E%3Cpath d='M50 50c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10s-10-4.477-10-10 4.477-10 10-10zM10 10c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10S0 25.523 0 20s4.477-10 10-10zm10 8c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm40 40c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+	}
+</style>
