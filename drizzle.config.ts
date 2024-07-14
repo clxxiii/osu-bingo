d95970279ea5b04552978e885617508e768f1aa9
clxxiii <clxxiii@users.noreply.github.com>
2024-07-14 16:26:37 -0400

    get rid of turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   dialect: 'sqlite',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
-    authToken: process.env.TURSO_TOKEN
+    url: process.env.DATABASE_URL!,
   },
   schema: ['./src/lib/drizzle/schema.ts', './src/lib/drizzle/relations.ts'],
   out: './src/lib/drizzle/migrations'
--- src/lib/drizzle/index.ts
+++ src/lib/drizzle/index.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@libsql/client';
-import { DATABASE_URL, TURSO_TOKEN } from '$env/static/private';
+import { DATABASE_URL } from '$env/static/private';
 import { drizzle } from 'drizzle-orm/libsql';
 import { EventEmitter } from "events"
 
 
-const sqlite = createClient({ url: DATABASE_URL, authToken: TURSO_TOKEN });
+const sqlite = createClient({ url: DATABASE_URL });
 export const db = drizzle(sqlite);
 
 export const emitter = new EventEmitter()
