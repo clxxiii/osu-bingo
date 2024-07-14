64f214bac671de3307399b4b122e2866481a530c
clxxiii <clxxiii@users.noreply.github.com>
2024-07-14 12:29:17 -0400

    prefix env variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   dialect: 'sqlite',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
-    authToken: process.env.TURSO_TOKEN
+    url: process.env.VITE_DATABASE_URL,
+    authToken: process.env.VITE_TURSO_TOKEN
   },
   schema: ['./src/lib/drizzle/schema.ts', './src/lib/drizzle/relations.ts'],
   out: './src/lib/drizzle/migrations'
