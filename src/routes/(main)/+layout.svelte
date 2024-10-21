b30a3880eafaef1496b37e7634ac28d00e0cc153
clxxiii <clxxiii@users.noreply.github.com>
2024-10-21 18:55:55 -0400

    move app css

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import '../../app.css';
+	import './app.css';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import type { PageData } from './$types';
