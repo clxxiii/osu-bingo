1137a91592c1877e0c8b27b0db006f5a18494e0f
clxxiii <clxxiii@users.noreply.github.com>
2025-03-10 15:36:14 -0400

    make login request more friendly

--- src/lib/components/LoginRequest.svelte
+++ src/lib/components/LoginRequest.svelte
@@ -12,11 +12,12 @@
 
 <div
 	transition:fly={{ delay: 500, y: 50, duration: 300 }}
-	class="h-full w-full bg-zinc-800/80 p-6 sm:max-h-[200px] sm:w-[400px] sm:rounded-xl sm:shadow-xl"
+	class="h-full w-full border-2 border-black/50 bg-zinc-800/80 p-6 sm:max-h-[220px] sm:w-[400px] sm:rounded-xl sm:shadow-xl"
 >
-	<h1 class="font-rounded text-2xl font-bold">Login Request</h1>
+	<h1 class="font-rounded text-2xl font-bold">Welcome back!</h1>
 	<hr class="my-1 rounded border-2 border-zinc-900" />
-	<p class="pb-4">We can't fetch scores from you! Please try logging in again</p>
+	<p class="pb-4">It's been a bit since you last played, so we can't fetch your scores.</p>
+	<p class="pb-4">If you log in again, it should all work fine!</p>
 	<button
 		class="w-full rounded-full border-pink-200 bg-pink-800 p-1 px-10 font-rounded font-bold transition hover:bg-pink-900 active:bg-pink-950"
 		on:click={login}>Log in</button
