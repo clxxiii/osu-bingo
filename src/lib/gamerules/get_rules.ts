5b483ced5e32bcd38af59eb73084863f2f32fc40
clxxiii <clxxiii@users.noreply.github.com>
2025-04-06 12:11:06 -0400

    change claim condition over time

--- src/lib/gamerules/get_rules.ts
+++ src/lib/gamerules/get_rules.ts
@@ -20,11 +20,25 @@ export const getRules = (game: Bingo.Card, squareIdx?: number): GameRules => {
     rules.rank = options.setup.rank;
   }
 
+  // TODO: Change rules if square is in a block
+
+  if (!game.start_time) return rules;
+
   // TODO: Get current Claim Condition according to events
+  const seconds_into_game = (Date.now() - new Date(game.start_time).valueOf()) / 1000
+
+  for (const event of options.event) {
+    if (event.seconds_after_start < seconds_into_game) {
+      if (event.event == 'claimchange') {
+        rules.claim_condition = event.detail;
+      } else if (event.event == 'reclaimchange') {
+        rules.reclaim_condition = event.detail;
+      }
+    }
+  }
 
   if (!squareIdx) return rules
 
-  // TODO: Change rules if square is in a block
 
   return rules;
 }
